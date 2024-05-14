095505fb27b9c82d85e0ceb416afddc963b6e466
Umar's <umarzaman124@gmail.com>
2024-05-14 15:48:59 +0500

    all

---
+++ src/library/db.js
@@ -0,0 +1,2 @@
+const {username,password}=process.env
+const connectionstr="mongodb+srv://"+username+":"+password+"@tadrees.cmesljr.mongodb.net/tadrees-db?retryWrites=true&w=majority&appName=Tadrees"
--- src/pages/api/postData.ts
+++ src/pages/api/postData.ts
@@ -1,43 +1,26 @@
-// pages/api/postData.js
+const mongoose = require("mongoose");
+const express = require("express")
 
-import mysql from 'mysql2/promise';
-
-// Function to establish a MySQL connection
-const bcrypt = require('bcryptjs');
-async function connectToDatabase() {
-  const connection = await mysql.createConnection({
-    host: '127.0.0.1',  // Change to your MySQL host
-    user: 'root',  // Change to your MySQL username
-    password: 'root',  // Change to your MySQL password
-    database: 'tadrees',  // Change to your MySQL database name
+const contactSchema = {
+  email: String,
+  query: String,
+};
+const Contact = mongoose.model("Contact", contactSchema);
+const app = express();
+mongoose.connect("mongodb://localhost:27017/collectionName", {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+});
+app.post("/contact", function (req, res) {
+  const contact = new Contact({
+      email: req.body.email,
+      query: req.body.query,
   });
-  return connection;
-}
-
-export default async function handler(req, res) {
-  
-  if (req.method === 'POST') {
-    // Assuming JSON data is sent in the request body
-    // const { data } = req.body;
-
-    try {
-      // Connect to the database
-      const connection = await connectToDatabase();
-
-      // Insert data into a table
-      const [rows, fields] = await connection.execute(
-       'insert INTO  tadrees.users (users.username,users.email) VALUES ("umer","umer@gmail.com");'
-      );
-
-      // Respond with a success message
-      res.status(200).json({ message: 'Data inserted successfully!' });
-    } catch (error) {
-      console.error('Error inserting data:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  } else {
-    // Method not allowed
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
+  contact.save(function (err) {
+      if (err) {
+          res.redirect("/error");
+      } else {
+          res.redirect("/thank-you");
+      }
+  });
+});
